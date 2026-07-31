31a71684928e56be8ae87ba379ca005487cebe39
Arthur Nieuwland <arthur.nieuwland@surf.nl>
2026-07-31 17:58:50 +0200

    Added camera info extraction. Unfortunately no IR lens info

--- src/parsing/zip/format/mod.rs
+++ src/parsing/zip/format/mod.rs
@@ -1,7 +1,9 @@
 mod calibration_data;
+mod camera_info;
 mod image_properties;
 mod ir_data;
 mod ir_image_info;
 
+pub use camera_info::{CameraInfo, extract_camera_info};
 pub use ir_data::{IrData, extract_ir_data, extract_ir_dimensions};
 pub use ir_image_info::{IrImageInfo, TemperatureScale, extract_ir_image_info};
