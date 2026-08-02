39d268f9d1fd378821705213bbb08524dc4f1145
Arthur Nieuwland <anieuwland@nimmerfort.eu>
2026-08-02 03:13:53 +0200

    Clean of warnings

--- src/parsing/zip/format/mod.rs
+++ src/parsing/zip/format/mod.rs
@@ -7,4 +7,4 @@ mod ir_image_info;
 pub use calibration_data::{CalibrationData, extract_calibration_data};
 pub use camera_info::{CameraInfo, extract_camera_info};
 pub use ir_data::{IrData, extract_ir_data, extract_ir_dimensions};
-pub use ir_image_info::{IrImageInfo, TemperatureScale, extract_ir_image_info};
+pub use ir_image_info::{IrImageInfo, extract_ir_image_info};
